d0f9a2ad8e5049d5006171a59c499433012d5147
Alfonso6z <alfonso6z.w@gmail.com>
2022-12-07 09:38:40 -0600

    swing

---
+++ 9_Swing/src/App.java
@@ -0,0 +1,10 @@
+
+import layouts.*;
+
+public class App {
+    public static void main(String[] args){
+        // Crea un objeto de la clase MiVentana
+        GridBag g = new GridBag("BorderLayout");
+        g.setVisible(true);
+    }
+}
